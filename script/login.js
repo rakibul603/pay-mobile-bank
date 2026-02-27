65620c3f710ac5232871719de774936bcce482ae
rakib <rakibco66@gmail.com>
2026-02-27 19:45:31 +0600

    design home header section

--- script/login.js
+++ script/login.js
@@ -1,1 +1,22 @@
-console.log('login page')
+document.getElementById('login-btn').addEventListener('click', function () {
+    console.log('login btn click')
+    const numberInput = document.getElementById('input-number');
+    const contactNumber = numberInput.value;
+
+    const passwordInput = document.getElementById('input-password')
+    const password = passwordInput.value;
+
+
+    if (contactNumber === '01234567890' && password === '1234') {
+        // window.location.replace('/home.html')
+        alert('login success')
+        window.location.assign('/home.html')
+    }
+
+    else {
+        alert("login Failed")
+    }
+    return;
+
+    console.log(contactNumber, password)
+})
